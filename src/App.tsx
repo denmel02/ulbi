8f715b5042f88341303274d5ad2812ffaae1d53b
Denis Melnikov <denmel02@gmail.com>
2023-02-07 15:10:50 +0300

    classNames

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,13 @@ import { Link, Route, Routes } from 'react-router-dom';
 import './styles/index.scss';
 import { AboutPageLazy, MainPageLazy } from './pages';
 import { useTheme } from './theme';
+import { classNames } from './helpers';
 
 export const App = () => {
   const { theme, toggleTheme } = useTheme();
 
   return (
-    <div className={ `app ${theme}` }>
+    <div className={ classNames('app', theme) }>
       <button onClick={ toggleTheme }>Toggle theme</button>
       <Link to="/">Main</Link>
       <Link to="/about">About</Link>
