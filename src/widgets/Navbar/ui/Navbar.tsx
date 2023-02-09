d87c9c2b7182aee858b635a1af77a3b5a76b39e1
Denis Melnikov <denmel02@gmail.com>
2023-02-09 23:52:37 +0300

    sidebar

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,10 +1,9 @@
-import { RouterLink, ToggleThemeButton } from "shared";
+import { RouterLink } from "shared";
 import classes from "./Navbar.module.scss";
 
 export const Navbar = () => {
   return (
     <div className={ classes.navbar}>
-      <ToggleThemeButton />
       <div className={ classes.links }>
         <RouterLink to="/" variant="secondary">Main</RouterLink>
         <RouterLink to="/about" variant="secondary">About</RouterLink>
